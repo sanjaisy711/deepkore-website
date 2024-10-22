3e91653c92a3bff1a9507c531a994c52d8459c84
sanjaisy711 <sanjaip711@gmail.com>
2024-10-22 10:25:32 +0530

    Update page.js

--- src/app/auth-signup/page.js
+++ src/app/auth-signup/page.js
@@ -22,8 +22,12 @@ export default function Page() {
                                     </div>
 
                                     <div className="mb-4">
-                                        <label className="font-semibold" htmlFor="LoginEmail">Email Address:</label>
+                                        <label className="font-semibold" htmlFor="LoginEmail">Business Email Address:</label>
                                         <input id="LoginEmail" type="email" className="form-input mt-3 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0" placeholder="name@example.com" />
+                                    </div>
+                                    <div className="mb-4">
+                                        <label className="font-semibold" htmlFor="LoginPhone">Phone :</label>
+                                        <input id="LoginEmail" type="email" className="form-input mt-3 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0" placeholder="" />
                                     </div>
 
                                     <div className="mb-4">
