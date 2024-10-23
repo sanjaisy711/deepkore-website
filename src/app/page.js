90a35201cc7d00f92a631f45c547a8b6cf8d0e26
sanjaisy711 <sanjaip711@gmail.com>
2024-10-23 17:34:39 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -54,7 +54,7 @@ export default function Page() {
                       placeholder="Your Email Address :"
                     />
                     <button
-                      type="submit" 
+                      type="submit" href="/contactus"  
                       className="py-2 px-5 inline-flex items-center item-center font-semibold tracking-wide align-middle transition duration-500 ease-in-out text-base text-center absolute top-[2px] end-[3px] h-[46px] bg-indigo-600 hover:bg-indigo-700 border border-indigo-600 hover:border-indigo-700 text-white rounded-full"
                         >
                       Schedule a Demo
