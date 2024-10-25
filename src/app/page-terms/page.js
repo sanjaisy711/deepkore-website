23964364819d749d0e7fa4f6fe59596fac09d8ce
sanjaisy711 <sanjaip711@gmail.com>
2024-10-25 09:32:08 +0530

    Update page.js

--- src/app/page-terms/page.js
+++ src/app/page-terms/page.js
@@ -45,12 +45,12 @@ export default function Page() {
                                 <p className="text-slate-400">Welcome to Dgiverse. These Terms of Service (“Terms”) govern your access to and use of our website, services, and platform (collectively, the “Services”). By accessing or using the Services, you agree to be bound by these Terms. If you do not agree to these Terms, please do not use the Services.</p>
 
                                 <h5 className="text-xl font-semibold mb-4 mt-8">1. Acceptance of Terms:</h5>
-                                <p className="text-slate-400">By using our platform and Services, you agree to these <b className="text-red-600">Terms</b> and any additional terms that may apply. You must be at least <b className="text-red-600">18 years </b> old to use our Services. If you are using our <b className="text-red-600">Services</b> on behalf of an organization, you are agreeing to these <b className="text-red-600"> Terms </b> on behalf of that organization.</p>
- 
+                                <p className="text-slate-400">By using our platform and Services, you agree to these Terms and any additional terms that may apply. You must be at least 18 years old to use our Services. If you are using our  Services on behalf of an organization, you are agreeing to these Terms on behalf of that organization.</p>
+
 
 
                                   <h5 className="text-xl font-semibold mb-4 mt-8">2. Changes to Terms:</h5>
-                                <p className="text-slate-400">We reserve the right to update or modify these <b className="text-red-600">Terms</b> at any time. Any changes will be effective immediately upon posting the revised <b className="text-red-600">version</b> on our website. Your continued use of the <b className="text-red-600">Services</b> after such changes constitutes your acceptance of the new Terms. Please review these Terms periodically for updates.</p>
+                                <p className="text-slate-400">We reserve the right to update or modify these Terms at any time. Any changes will be effective immediately upon posting the revised version on our website. Your continued use of the Services after such changes constitutes your acceptance of the new Terms. Please review these Terms periodically for updates.</p>
 
 
 
@@ -58,7 +58,7 @@ export default function Page() {
 
 
                                    <h5 className="text-xl font-semibold mb-4 mt-8">3. Services Overview:</h5>
-                                <p className="text-slate-400">Our platform enables users to create, manage, and deploy applications using <b className="text-red-600"> low-code/no-code </b> tools. The specific services we offer may include</p>
+                                <p className="text-slate-400">Our platform enables users to create, manage, and deploy applications using low-code/no-code tools. The specific services we offer may include</p>
                                    <ul className="list-none text-slate-400 mt-3">
                                     <li className="flex items-center mt-2"><FaArrowRight className="ms-2 text-[10px] text-indigo-600  align-middle me-2"/>Application Development-Tools to build applications without extensive coding.</li>
                                     <li className="flex items-center mt-2"><FaArrowRight className="ms-2 text-[10px] text-indigo-600  align-middle me-2"/>Customization and Automation-Features to automate processes and customize workflows.</li>
@@ -69,14 +69,14 @@ export default function Page() {
 
 
                                   <h5 className="text-xl font-semibold mb-4 mt-8">4. User Accounts:</h5>
-                                <p className="text-slate-400">To access certain features of our <b className="text-red-600"> Services </b> , you may be required to create an account. You agree to:</p>
+                                <p className="text-slate-400">To access certain features of our Services , you may be required to create an account. You agree to:</p>
                                    <ul className="list-none text-slate-400 mt-3">
                                     <li className="flex items-center mt-2"><FaArrowRight className="ms-2 text-[10px] text-indigo-600  align-middle me-2"/>Provide accurate, current, and complete information when registering.</li>
                                     <li className="flex items-center mt-2"><FaArrowRight className="ms-2 text-[10px] text-indigo-600  align-middle me-2"/>Maintain the security of your account and password.</li>
                                     <li className="flex items-center mt-2"><FaArrowRight className="ms-2 text-[10px] text-indigo-600  align-middle me-2"/>Notify us immediately of any unauthorized use of your account.</li>
                                     <li className="flex items-center mt-2"><FaArrowRight className="ms-2 text-[10px] text-indigo-600  align-middle me-2"/>Be responsible for all activities that occur under your account.</li>
                                 </ul> 
-                                <p className="text-slate-400">We reserve the right to terminate accounts that provide false or misleading information or violate these<b className="text-red-600"> Terms</b>.</p>
+                                <p className="text-slate-400">We reserve the right to terminate accounts that provide false or misleading information or violate these Terms.</p>
 
 
 
@@ -97,21 +97,21 @@ export default function Page() {
 
 
                                 <h5 className="text-xl font-semibold mb-4 mt-8">6. Fees and Payment</h5>
-                                <p className="text-slate-400">Some of our <b className="text-red-600"> Services </b> may be provided on a paid basis. By selecting a paid plan or subscription, you agree to pay the fees associated with that plan. All fees are non-refundable unless otherwise specified in writing. We reserve the right to change our pricing at any time with reasonable notice.</p>
+                                <p className="text-slate-400">Some of our Services may be provided on a paid basis. By selecting a paid plan or subscription, you agree to pay the fees associated with that plan. All fees are non-refundable unless otherwise specified in writing. We reserve the right to change our pricing at any time with reasonable notice.</p>
 
 
                                      <h5 className="text-xl font-semibold mb-4 mt-8">7. Intellectual Property</h5>
                                      <h3 className="text-lg font-semibold mb-4 mt-8">7.1. Ownership:</h3>
-                                <p className="text-slate-400">All content, technology, and intellectual property <b className="text-red-600"> rights </b> on our platform, including software, logos, and designs, are owned by or licensed to us. You agree not to copy, distribute, or create derivative works based on our platform without prior written permission.</p>
+                                <p className="text-slate-400">All content, technology, and intellectual property rights on our platform, including software, logos, and designs, are owned by or licensed to us. You agree not to copy, distribute, or create derivative works based on our platform without prior written permission.</p>
 
                                   <h3 className="text-lg font-semibold mb-4 mt-8">7.2. Your Content:</h3>
-                                <p className="text-slate-400">You retain ownership of any content, data, or applications you create using our platform. By using our Services, you grant us a worldwide, royalty-free license to use, host, store, reproduce, and display your content solely for the purpose of providing the<b className="text-red-600"> Services </b> to you.</p>
+                                <p className="text-slate-400">You retain ownership of any content, data, or applications you create using our platform. By using our Services, you grant us a worldwide, royalty-free license to use, host, store, reproduce, and display your content solely for the purpose of providing the Services to you.</p>
 
 
 
 
                                      <h5 className="text-xl font-semibold mb-4 mt-8">8. Third-Party Services</h5>
-                                <p className="text-slate-400">Our platform may integrate with or allow you to use third-party services. These third-party <b className="text-red-600"> services </b>are not controlled by us, and their use is subject to their own terms and privacy policies. We are not responsible for the content, accuracy, or reliability of any third-party services.</p>
+                                <p className="text-slate-400">Our platform may integrate with or allow you to use third-party services. These third-party services are not controlled by us, and their use is subject to their own terms and privacy policies. We are not responsible for the content, accuracy, or reliability of any third-party services.</p>
 
 
 
@@ -121,7 +121,7 @@ export default function Page() {
 
 
                                     <h5 className="text-xl font-semibold mb-4 mt-8">10. Disclaimer of Warranties</h5>
-                                <p className="text-slate-400">The<b className="text-red-600"> Services </b> are provided "as is" and "as available" without warranties of any kind, whether express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, non-infringement, or course of performance.</p>
+                                <p className="text-slate-400">The Services are provided "as is" and "as available" without warranties of any kind, whether express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, non-infringement, or course of performance.</p>
                                 <p className="text-slate-400">We do not warrant that:</p>
                                  <ul className="list-none text-slate-400 mt-3">
                                     <li className="flex items-center mt-2"><FaArrowRight className="ms-2 text-[10px] text-indigo-600  align-middle me-2"/>The Services will function uninterrupted, secure, or error-free.</li>
@@ -147,11 +147,11 @@ export default function Page() {
 
 
                                       <h5 className="text-xl font-semibold mb-4 mt-8">13. Governing Law</h5>
-                                <p className="text-slate-400">These Terms will be governed by and construed in accordance with the laws of [Jurisdiction], without regard to its conflict of law principles. Any legal action or proceeding arising under these<b className="text-red-600"> Terms </b> will be brought exclusively in the courts located in [Jurisdiction], and you hereby consent to the jurisdiction of such courts.</p>
+                                <p className="text-slate-400">These Terms will be governed by and construed in accordance with the laws of [Jurisdiction], without regard to its conflict of law principles. Any legal action or proceeding arising under these Terms will be brought exclusively in the courts located in [Jurisdiction], and you hereby consent to the jurisdiction of such courts.</p>
 
 
                                  <h5 className="text-xl font-semibold mb-4 mt-8">14. Contact Us</h5>
-                                <p className="text-slate-400">If you have any questions about these<b className="text-red-600"> Terms </b> or the <b className="text-red-600"> Services </b> , please contact us at:<Link href="#" className="relative inline-flex items-center font-semibold tracking-wide align-middle text-base text-center border-none after:content-[''] after:absolute after:h-px after:w-0 hover:after:w-full after:end-0 hover:after:end-auto after:bottom-0 after:start-0 after:duration-500 text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 duration-500">contact@worksbyte.com</Link></p>
+                                <p className="text-slate-400">If you have any questions about these Terms or the Services , please contact us at:<Link href="#" className="relative inline-flex items-center font-semibold tracking-wide align-middle text-base text-center border-none after:content-[''] after:absolute after:h-px after:w-0 hover:after:w-full after:end-0 hover:after:end-auto after:bottom-0 after:start-0 after:duration-500 text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 duration-500">contact@worksbyte.com</Link></p>
 
 
         
