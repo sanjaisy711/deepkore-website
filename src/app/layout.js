61cc49da51111c44df92f3f8bc639c806b371d0f
sanjaisy711 <sanjaip711@gmail.com>
2024-11-19 11:22:08 +0530

    comment

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,53 +1,60 @@
-import "../app/assets/scss/tailwind.scss"
-import './assets/css/material.css'
-import { Nunito, Work_Sans, EB_Garamond, Kaushan_Script, Alex_Brush } from 'next/font/google'
-
+import "../app/assets/scss/tailwind.scss";
+import "./assets/css/material.css";
+import {
+  Nunito,
+  Work_Sans,
+  EB_Garamond,
+  Kaushan_Script,
+  Alex_Brush,
+} from "next/font/google";
 
 const nunito = Nunito({
-  subsets: ['latin'],
-  variable: '--font-nunito',
-  weight: ['300','400','500','600', '700'],
-  display: 'swap',
-})
+  subsets: ["latin"],
+  variable: "--font-nunito",
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
 const work_sans = Work_Sans({
-  subsets: ['latin'],
-  variable: '--font-work-sans',
-  weight: ['300','400','500','600', '700'],
-  display: 'swap',
-})
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
 const eb_garamond = EB_Garamond({
-  subsets: ['latin'],
-  variable: '--font-eb-garamond',
-  weight: ['400'],
-  display: 'swap',
-})
-
+  subsets: ["latin"],
+  variable: "--font-eb-garamond",
+  weight: ["400"],
+  display: "swap",
+});
 
 const kaushan = Kaushan_Script({
-  subsets: ['latin'],
-  variable: '--font-kaushan',
-  weight: ['400'],
-  display: 'swap',
-})
-
+  subsets: ["latin"],
+  variable: "--font-kaushan",
+  weight: ["400"],
+  display: "swap",
+});
 
 const alex = Alex_Brush({
-  subsets: ['latin'],
-  variable: '--font-alex',
-  weight: ['400'],
-  display: 'swap',
-})
-
+  subsets: ["latin"],
+  variable: "--font-alex",
+  weight: ["400"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: 'Worksbyte - One platform. No-code. Hassle-free',
-  description: 'Worksbyte - One platform. No-code. Hassle-free',
-}
+  title: "Worksbyte - Build Smarter, Deploy Faster",
+  description:
+    "Worksbyte lets you transform ideas into powerful Enterprise applications that adapt to your business needs.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" dir="LTR">
-      <body className={`${nunito.variable} ${work_sans.variable} ${eb_garamond.variable} ${kaushan.variable} ${alex.variable} font-nunito text-base text-black dark:text-white dark:bg-slate-900`}>{children}</body>
+      <body
+        className={`${nunito.variable} ${work_sans.variable} ${eb_garamond.variable} ${kaushan.variable} ${alex.variable} font-nunito text-base text-black dark:text-white dark:bg-slate-900`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
